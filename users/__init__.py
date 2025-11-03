0208227a55433f58a6989b073a43adac170e3ef1
Alinash2024 <interspeis@gmail.com>
2025-11-03 12:04:57 +0300

    feat(shop_graphql): Cleaned up to keep only users app files

--- users/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-The `users` package for the Django application.
-
-This package contains models, views, forms, and other components related to
-managing user accounts, authentication, and authorization.
-"""
--- users/apps.py
+++
@@ -1,8 +0,0 @@
-"""App configuration for the users application."""
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    """App config for the users app."""
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "users"
